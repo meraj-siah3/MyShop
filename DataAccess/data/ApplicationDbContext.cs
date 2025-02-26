f4cc4cd8e7459480eac2a7310b249abb34228c15
Meraj <siahtirimeraj@gmail.com>
2025-02-27 00:09:13 +0330

    SeedData

--- DataAccess/data/ApplicationDbContext.cs
+++ DataAccess/data/ApplicationDbContext.cs
@@ -16,5 +16,13 @@ namespace DataAccess.data
 
         }
         public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "action", DisplayOrder = 1 },
+            new Category { Id = 2, Name = "scifi", DisplayOrder = 2 },
+            new Category { Id = 3, Name = "history", DisplayOrder = 3 }
+            );
+        }
     }
 }
