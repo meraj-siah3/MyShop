d0d4498a618ada73665e1b6c72ce271868a200e6
Meraj <siahtirimeraj@gmail.com>
2025-03-07 23:44:13 +0330

    update controller and view for category

--- MyShop/Controllers/CategoryController.cs
+++ MyShop/Controllers/CategoryController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess.data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.Models;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MyShop.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+       
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+            
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
