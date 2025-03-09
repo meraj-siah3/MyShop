2d4185bfdb79bdae4d486c3574fc674d0fb5d716
Meraj <siahtirimeraj@gmail.com>
2025-03-09 20:23:47 +0330

    createedit controller(

--- MyShop/Controllers/CategoryController.cs
+++ MyShop/Controllers/CategoryController.cs
@@ -11,11 +11,11 @@ namespace MyShop.Controllers
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _db;
-       
+
         public CategoryController(ApplicationDbContext db)
         {
             _db = db;
-            
+
         }
         public IActionResult Index()
         {
@@ -30,6 +30,33 @@ namespace MyShop.Controllers
 
         [HttpPost]
         public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? CategoryDb = _db.Categories.Find(id);
+            if (CategoryDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
         {
             if (ModelState.IsValid)
             {
