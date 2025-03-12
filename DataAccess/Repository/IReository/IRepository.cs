5e67b909eb98c85ab83cd681abfb004f68db4d89
Meraj <siahtirimeraj@gmail.com>
2025-03-12 21:33:49 +0330

    IRepo,Repo  Create

---
+++ DataAccess/Repository/IReository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository.IReository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+
+
+
+
+    }
+}
