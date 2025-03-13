1d46ce0dde55fd6d363bf2a6653e01eb60dbd3ee
Meraj <siahtirimeraj@gmail.com>
2025-03-13 14:15:35 +0330

    ICategoryRepo,CategoryRepo  Done

---
+++ DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.data;
+using DataAccess.Repository.IReository;
+using Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
