e4ea103d919a043f56e28a4e1aab4c47c04f6f49
Meraj <siahtirimeraj@gmail.com>
2025-03-27 13:40:35 +0330

    fix home page

--- MyShop/Areas/Customer/Controllers/HomeController.cs
+++ MyShop/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
+using Models.Models;
 using MyShop.Models;
 using System.Diagnostics;
 
@@ -8,15 +10,21 @@ namespace MyShop.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfwork;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfwork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> ProductList = _unitOfwork.Product.GetAll(IncludeProperties: "Category");
+
+            return View(ProductList);
+        
         }
 
         public IActionResult Privacy()
